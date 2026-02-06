0d72f71442c41ec9d228f821ae09ea14d2ae0df2
Brian Obot <brianobot@gmail.com>
2026-02-06 01:07:24 +0100

    Resolve workspace issue with Workspace for rust programs

--- _2024/day_3.rs
+++ _2024/day_3.rs
@@ -1,42 +1,41 @@
+#![allow(unused_variables, unused_mut)]
+
 use std::fs;
 
-
-fn is_valid_instruction(part: String) -> Option<bool> {
-    
+fn is_valid_instruction(_part: String) -> Option<bool> {
+    None
 }
 
 fn extract_instructions(data: String) -> Vec<String> {
     let mut instructions = Vec::new();
     let mut do_instruction = true;
-    
+
     let mut instruction_parts = String::new();
     let mut current_instruction = String::new();
-    
+
     let special_chars = ['m', 'u', 'l', '(', ')', ',', '-'];
-    
+
     for char in data.chars() {
         if special_chars.contains(&char) {
             current_instruction.push(char);
         } else {
-            current_instruction = "";
+            current_instruction = "".to_string();
         }
     }
-    
-    valid_instruction = is_valid_instruction(current_instruction);
-    
-    match valid_instruction {
-        
-    }
-    
-    instructions
-    
-}
 
+    let valid_instruction = is_valid_instruction(current_instruction);
+
+    if valid_instruction.is_some() {
+        println!("Instruction is Valid");
+    }
+
+    instructions
+}
 
 fn main() {
     let data = fs::read_to_string("_2024/data/day_3_input.txt").expect("Failed to Read Input File");
-    
+
     println!("Data = {data}");
-    
+
     let instructions = extract_instructions(data);
 }
